68e3a22ec032338077b973156a14e205db606585
sudhirkumartri <skt06678@gmail.com>
2025-06-11 16:43:19 +0530

    Update SampleClass.java

--- src/test/java/practiceProject1/SampleClass.java
+++ src/test/java/practiceProject1/SampleClass.java
@@ -3,6 +3,7 @@ package practiceProject1;
 public class SampleClass {
 public static void main(String[] args) {
 	System.out.println("this main method");
+	System.out.println("this is new line");
 }
 	
 }
